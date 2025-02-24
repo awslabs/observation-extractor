9cef60d8c41594703b2753343b2c6b097fec5959
Josh Famestad <famestad@amazon.com>
2025-02-24 13:39:05 -0800

    added copyright header

--- observer/ingestors/pdf.py
+++ observer/ingestors/pdf.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 import os
 from pdf2image import convert_from_path
 import base64
--- observer/writers/csv.py
+++ observer/writers/csv.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 import csv
 
 from observer.model.observations import Observation
